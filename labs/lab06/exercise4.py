d3fe93f52d18b8f182f1007e09cb61a6c73e86f8
ALWAFIA <alwafiabdulgader@gmail.com>
2025-08-11 12:16:05 +0800

    comment on ex.4 (\t)

--- labs/lab06/exercise4.py
+++ labs/lab06/exercise4.py
@@ -1,7 +1,11 @@
-receipt = "========== RECEIPT ==========\nItem\tPrice\tQty\tTotal\n\nCoffee\t$3.50\t2\t$7.00\n\n============================"
-print(receipt)
+Item = input("Enter Item: ")
+Price = float(input("Enter Price: "))
+Qty = int(input("Enter Quantity: "))
+Total = float(Price * Qty)
+print(f"========== RECEIPT ==========\nItem\tPrice\tQty\tTotal\n\n{Item}\t${Price}\t{Qty}\t${Total}\n============================")
 
-
+"""SPACES IN OUTPUT NOT CONSISTENT BECAUSE TERMINAL KASI CANTIK, 
+MUNGKIN TERMINAL LAIN 'TAK CANTIK MACAM TU'"""
 
 
 
