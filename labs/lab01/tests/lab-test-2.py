a749ab9aad5a0a6be4965324eb8a57be391671bf
ALWAFIA <alwafiabdulgader@gmail.com>
2025-08-18 12:02:35 +0800

    done
    done

---
+++ labs/lab01/tests/lab-test-2.py
@@ -0,0 +1,6 @@
+"""Problem description:
+'Write a complete Python program that uses an appropriate esacape characters and arithmetic expression to produce the output as below'"""
+Marks = "2x10"
+Full_Marks = 2 * 10
+print(f"Name: Alwafi Abdugader Alamin\t\t\tMatric. No: MC2515202741\n*\t\t\t*\n**\t\t**\n***\t***\n********\n***\t***\n**\t\t**\n*\t\t\t*\n\nThis is my\n\tsecond\n\t\tassignment\nI want {Marks}, which is {Full_Marks} full marks")
+#Space \t was not accurate in "assignment" section
