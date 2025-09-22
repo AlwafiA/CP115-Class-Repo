cb1c39fe86c0d4e79597ee23b4c261ed40d154e0
AlwafiA <alwafiabdulgader@gmail.com>
2025-09-22 11:30:24 +0800

    commen

--- labs/lab09/exercise3/exercise3.py
+++ labs/lab09/exercise3/exercise3.py
@@ -3,34 +3,32 @@ show_time = int(input())
 customer_type = input()
 is_student = input()
 
-#CONSTANTS
-base_price = 0
-student_discount = 0.1
-final_price = (base_price) * (1 - student_discount)
-
-# DETERMINE TICKET PRICE    
-if day_type == "Weekends":
-    student_discount = 0
+# Determine base price only
+if day_type == "Weekend":
     if customer_type == "Adult":
         base_price = 18
     elif customer_type == "Child":
         base_price = 12
     elif customer_type == "Senior":
         base_price = 15
-    if show_time > 1800:
-        new_base = base_price + 3
-elif day_type == "Weekdays":
+elif day_type == "Weekday":
     if customer_type == "Adult":
         base_price = 15
     elif customer_type == "Child":
         base_price = 10
     elif customer_type == "Senior":
         base_price = 12
-    if show_time > 1800:
-        new_base = base_price + 3
-if is_student == "Yes":
-    student_discount = 0.1
-final_price = (base_price) * (1 - student_discount)
 
+# Final price starts as base
+final_price = base_price
+
+# Evening surcharge
+if show_time >= 19:
+    final_price += 3
+
+# Student discount (only weekdays)
+if day_type == "Weekday" and is_student == "Yes":
+    final_price *= 0.9
+# Output results
 print(base_price)
 print(final_price)
--- labs/lab09/exercise4/exercise4.py
+++ labs/lab09/exercise4/exercise4.py
@@ -1,9 +1,21 @@
 current_reading = int(input())
 previous_reading = int(input())
 
-# TODO your code here
+consumption = current_reading - previous_reading
 
+# Calculate water cost based on consumption
+if consumption <= 20:
+    water_cost = 0.57 * consumption
+elif consumption <= 35:
+    water_cost = (0.57 * 20) + (1.03 * (consumption - 20))
+else:
+    water_cost = (0.57 * 20) + (1.03 * 15) + (1.40 * (consumption - 35))
+
+#plus service charges
+total_bill = water_cost + 8 + 2
 
 print(consumption)
-print(water_cost)
-print(total_bill)
+print((water_cost))
+print((total_bill))
+
+# adding a "current : " in input causes error
