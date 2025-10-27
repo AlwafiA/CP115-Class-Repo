2423e6ac1abd263912711c56c91f2885149e336e
ALWAFIA <alwafiabdulgader@gmail.com>
2025-10-27 12:12:32 +0800

    yes

--- labs/lab13/exercise1/exercise1.py
+++ labs/lab13/exercise1/exercise1.py
@@ -1,6 +1,14 @@
 correct_password = "python123"
+attempts_used = 0
+login_successful = False
+while attempts_used < 3 and not login_successful:
+    entered_password = input("Enter your password: ")
+    attempts_used += 1
+    if entered_password == correct_password:
+        login_successful = True
+        print("Login successful!")
+    else:
+        print("Incorrect password. Try again.")
 
-# TODO: Your code here
-
-print(login_successful)
-print(attempts_used)
+print("login_successful")
+print("attempts_used")
