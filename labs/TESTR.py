5f4ba48090202b8bdd16b868eb8082851ff3b6d8
ALWAFIA <alwafiabdulgader@gmail.com>
2026-04-03 08:14:34 +0800

    update

--- labs/TESTR.py
+++ labs/TESTR.py
@@ -1,13 +1,6 @@
-
-
-hours = int(input())
-
-while True:
-    if hours == 3:
-     if hours <= 0:
-         hours = int(input())
-         continue
-    
-    pay = hours * 12.50
-    print(pay)
-    hours = int(input())
+total = 0
+for day in range(1, 6):
+    amount = float(input("Enter amount of plastic collected on day " +
+str(day) + ": "))
+    total += amount
+print("Total amount of plastic collected in 5 days:",total)
